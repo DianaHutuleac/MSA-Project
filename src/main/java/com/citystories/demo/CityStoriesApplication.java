6ab7ffec7010994e0663d6f518d8b5bdd92e0c2e
DianaHutuleac <diana.hutuleac@student.upt.ro>
2024-10-29 17:13:52 +0200

    Initial spring setup

---
+++ src/main/java/com/citystories/demo/CityStoriesApplication.java
@@ -0,0 +1,13 @@
+package com.citystories.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityStoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityStoriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citystories/demo/CityStoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citystories.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityStoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
